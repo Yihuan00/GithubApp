1420f486bb0012aef8c8a7c6fca4806fafdc4199
Yihuan00 <fenglingxuewin@163.com>
2021-01-21 16:20:56 +0800

    first init

---
+++ app/src/main/java/com/yihuan/github/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.yihuan.github.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+        val userId: String,
+        val displayName: String
+)
---
+++ app/src/main/java/com/yihuan/github/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.yihuan.github.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
